19f8494220e4f6842284bfc654b582379ec9b4cb
AlixLejeune <alix.lejeune@etu.univ-smb.fr>
2024-02-16 16:31:48 +0100

    setup

---
+++ API Web ASP.NET Core/Models/EntityFramework/FilmDBContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
+
+namespace API_Web_ASP.NET_Core.Models.EntityFramework
+{
+    public partial class FilmsDBContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+        public FilmsDBContext()
+        {
+        }
+
+        public FilmsDBContext(DbContextOptions<FilmsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Notation> Categories { get; set; } = null!;
+        public virtual DbSet<Film> Films { get; set; } = null!;
+        public virtual DbSet<Utilisateur> Utilisateurs { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLoggerFactory(MyLoggerFactory).EnableSensitiveDataLogging().UseNpgsql("Server=localhost;port=5432;Database=FilmsAvis;uid= postgres; password=postgres;");
+                //optionsBuilder.UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Avi>(entity =>
+            {
+                entity.HasKey(e => new { e.Film, e.Utilisateur })
+                    .HasName("pk_avis");
+
+                entity.HasOne(d => d.FilmNavigation)
+                    .WithMany(p => p.Avis)
+                    .HasForeignKey(d => d.Film)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_avis_film");
+
+                entity.HasOne(d => d.UtilisateurNavigation)
+                    .WithMany(p => p.Avis)
+                    .HasForeignKey(d => d.Utilisateur)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_avis_utilisateur");
+            });
+
+            modelBuilder.Entity<Film>(entity =>
+            {
+                entity.HasOne(d => d.CategorieNavigation)
+                    .WithMany(p => p.Films)
+                    .HasForeignKey(d => d.Categorie)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_film_categorie");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
